3aeeee1e515fb3788a3f5cdd40e4850e516a3f1f
mishapodl <mpodlevskykh@gmail.com>
2019-07-03 00:02:50 +0300

    Аритектура файлов и папок готова

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Header</h1>
+    </header>
+  </div>
+);
+
+export default App;
