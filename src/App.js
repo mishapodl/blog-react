5db057c9b67c385f6d77d45901dcea8e2c8f4152
mishapodl <mpodlevskykh@gmail.com>
2019-07-03 23:33:00 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "./App.scss";
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <h1>Header</h1>
-    </header>
-  </div>
-);
+const App = () => <div>!!!</div>;
 
 export default App;
