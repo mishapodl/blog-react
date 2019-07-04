248bf11cff6407c0c58e090d19a72c986fad4017
mishapodl <mpodlevskykh@gmail.com>
2019-07-05 01:09:01 +0300

    Добавил стили хеду и футу

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,6 +9,14 @@ const Footer = () => {
         <Navigation showListItem={false} />
         <BarContacts showHaderSection={true} />
       </div>
+			<div class="footer-copy">
+        <div class="footer-conteiner">
+          <div class="footer-copyright">&copy; zazaend<span></span> 2019</div>
+          <div class="footer-developed-by">
+            <p>Developed by<strong>: Michael Podlevskykh</strong></p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
