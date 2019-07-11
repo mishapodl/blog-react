42e1beaef51031be0b93e4e6975964963024cb89
mishapod <mpodlevskykh@gmail.com>
2019-07-11 19:05:53 +0300

    ---

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,5 @@
+import { getPosts } from "./actions-posts";
+
+export {
+  getPosts
+}
--- client/src/redux/reducers/reducer-posts.js
+++ client/src/redux/reducers/reducer-posts.js
@@ -1,5 +1,34 @@
-function postsReducer( state = [], action ) {
-  return state;
+import { POSTS } from "../constants";
+
+const postState = {
+  posts: [],
+  loading: false,
+  success: false
 };
+
+function postsReducer(state = postState, action) {
+  switch (action.type) {
+    case POSTS.POSTS_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case POSTS.POSTS_SUCCESS:
+      return {
+        ...state,
+        posts: [...action.payload],
+        success: true,
+        isLoading: false,
+      };
+    case POSTS.POSTS_ERROR:
+      return {
+        ...state,
+        isLoading: true,
+        success: false,
+      };
+    default:
+      return state;
+  }
+}
 
 export default postsReducer;
