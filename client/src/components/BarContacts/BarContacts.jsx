8396a27294714dd12a0b0a0c2714fffa87b5b82c
mishapod <mpodlevskykh@gmail.com>
2019-07-31 02:41:57 +0300

    ---

--- client/src/components/BarContacts/BarContacts.jsx
+++ client/src/components/BarContacts/BarContacts.jsx
@@ -12,28 +12,26 @@ const BarContacts = ({ showHaderSection }) => {
       )}
 
       <ul>
-        <div>
-          <li>
-            <a href="mailto: mishapod@gmail.com">
-              <i className="icon far fa-envelope" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/mishapodl">
-              <i className="icon fab fa-github" />
-            </a>
-          </li>
-          <li>
-            <a href="https://ru.stackoverflow.com/users/252282/michael-podlevskykh">
-              <i className="icon fab fa-stack-overflow" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/michael-podlevskykh-574b98119/">
-              <i className="icon fab fa-linkedin" />
-            </a>
-          </li>
-        </div>
+        <li>
+          <a href="mailto: mishapod@gmail.com">
+            <i className="icon far fa-envelope" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/mishapodl">
+            <i className="icon fab fa-github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://ru.stackoverflow.com/users/252282/michael-podlevskykh">
+            <i className="icon fab fa-stack-overflow" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/michael-podlevskykh-574b98119/">
+            <i className="icon fab fa-linkedin" />
+          </a>
+        </li>
       </ul>
     </section>
   );
