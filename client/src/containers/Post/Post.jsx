0afbd270a0d60aa1e49a5f49b48c48d931d78609
mishapodl <mpodlevskykh@gmail.com>
2019-08-01 02:58:57 +0300

    Комментари к посту подгружаются даже с перезагрузк

--- client/src/containers/Post/Post.jsx
+++ client/src/containers/Post/Post.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { deleteComment, setComments } from "../../redux/actions/index";
+import {
+  deleteComment,
+  setComments,
+  getIdPost
+} from "../../redux/actions/index";
 import { Comments, Article } from "../../components/index";
 import "./Post.scss";
 
 class Post extends Component {
   render() {
     const {
+			idPost,
       posts,
       comments: { comments },
       auth,
       isLoad: { isLoadPosts, isLoadComments },
       deleteComment,
+      getIdPost,
       match: {
         params: { id_post }
       }
     } = this.props;
-    const currentPost = posts[+id_post];
+		const currentPost = posts[+id_post];
+		!idPost && isLoadPosts && getIdPost(currentPost._id);
 
-    return (
+		return (
       <main>
         <section>
           <article className="post">
@@ -51,21 +58,23 @@ Post.propTypes = {
   auth: PropTypes.object.isRequired,
   isLoad: PropTypes.object.isRequired,
   id_post: PropTypes.string.isRequired,
-  deleteComment: PropTypes.func.isRequired
+  deleteComment: PropTypes.func.isRequired,
+  getIdPost: PropTypes.func.isRequired
 };
 
 Post.defaultProps = {
   id_post: ""
 };
 
-const mapStateToProps = ({ posts, comments, isLoad, auth }) => ({
+const mapStateToProps = ({ posts, comments, isLoad, auth, idPost }) => ({
   posts,
   comments,
   isLoad,
-  auth
+	auth,
+	idPost
 });
 
 export default connect(
   mapStateToProps,
-  { deleteComment, setComments }
+  { deleteComment, setComments, getIdPost }
 )(Post);
