c2fe30a0baad882c92a30a7fc0bdbbe9e920b5fa
mishapodl <mpodlevskykh@gmail.com>
2019-08-01 02:59:24 +0300

    делаю подгрузку постов, пагинацию

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,9 +4,18 @@ const router = express.Router();
 const Post = require("../../models/Post");
 
 router.get("/", (req, res) => {
-  Post.find().then(posts => {
+  Post.find().limit(4).then(posts => {
     res.json(posts);
   });
 });
+
+// router.get("/posts?limit=:limit", (req, res) => {
+//   Post.find()
+//     .limit(parseInt(req.params.limit))
+//     .then(posts => {
+//       res.json(posts);
+//     })
+//     .catch(err => console.log(err));
+// });
 
 module.exports = router;
