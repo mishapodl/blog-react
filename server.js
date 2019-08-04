421ae1e23f79cf7c5bbc7f8654ed624bc226b711
mishapodl <mishapod@gmail.com>
2019-08-05 01:26:35 +0300

    Подготовка к хероку

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const config = require("config")
+const config = require("config");
+const config = require("path");
 const app = express();
 
 //Body
@@ -15,11 +16,21 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch(err => console.error(err));
 
-const port = process.env.PORT || 5000;
-
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/comments", require("./routes/api/comments"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
+
+// production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
